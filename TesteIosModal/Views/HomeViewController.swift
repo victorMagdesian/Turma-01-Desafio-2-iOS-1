104a594bd1e85bc0ed62acc997fb88b78a6b36f4
Victor Magdesian <Victor.magdesian@vaivoa.com>
2021-12-14 11:14:16 -0800

    init

---
+++ TesteIosModal/Views/HomeViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  TesteIosModal
+//
+//  Created by VICTOR FELIPPE MAGDESIAN on 14/12/21.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+
+}
+
